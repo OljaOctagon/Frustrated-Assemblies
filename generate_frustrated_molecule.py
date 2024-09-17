8e4bb9f93efc71859d533d777a856d0533cfef38
Cat Nate <catnat@protonmail.com>
2024-09-17 18:20:11 +0200

    fixed some bugs in starting state generation script

--- generate_frustrated_molecule.py
+++ generate_frustrated_molecule.py
@@ -17,21 +17,21 @@ r = sigma / 2
 alpha1 = np.pi/2 - beta1*np.pi/180
 alpha2 = np.pi/2 - beta2*np.pi/180
 
-x1 = np.cos(alpha1)/r 
-y1 = np.sin(alpha1)/r 
+x1 = np.cos(alpha1)*r 
+y1 = np.sin(alpha1)*r 
 
-x2 = np.cos(alpha2)/r
-y2 = np.sin(alpha2)/r
+x2 = np.cos(alpha2)*r
+y2 = np.sin(alpha2)*r
 
 npart=5
 pos = np.zeros((npart,3))
 pos[1,:] = np.array([-x1,y2,0])
 pos[2,:] = np.array([x1,y1,0])
-pos[3,:] = np.array([x2,y2,0])
-pos[4,:] = np.array([-x2,y2,0])
+pos[3,:] = np.array([x2,-y2,0])
+pos[4,:] = np.array([-x2,-y2,0])
 
 print(pos)
-ptype = np.array([1,2,2,3,3])
+ptype = np.array([1,2,3,3,2])
 
 atoms=[]
 types=[]
